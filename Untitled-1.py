972a41d51235f1de4a83885e245706b3b307c535
mohammadjavvadmohammadi <149230041+mohammadjavvadmohammadi@users.noreply.github.com>
2024-10-12 10:14:02 +0330

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+a =  int(input("enter your first number:"))
+b = int(input("enter your second number:"))
+c = a + b
+print(c)
+
+e =  int(input("enter your first number:"))
+f = int(input("enter your second number:"))
+g = e * f
+print(g)
+
+h =  int(input("enter your first number:"))
+i = int(input("enter your second number:"))
+j = h -i
+print(j)
+
+k =  int(input("enter your first number:"))
+l = int(input("enter your second number:"))
+m = k / l
+print(m)
